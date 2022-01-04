dd8313650d8738d8b3a4e0249297822a6da61f93
Alina Zhechkova <alinazhech@gmail.com>
2022-01-04 21:48:55 +0200

    Added mobile version of users list

--- src/components/Welcome/UserList/index.tsx
+++ src/components/Welcome/UserList/index.tsx
@@ -1,4 +1,10 @@
-import React, { SetStateAction, useContext, useEffect, useState } from "react";
+import React, {
+  SetStateAction,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 
 import { auth, db } from "../../../firebase";
 
@@ -7,6 +13,8 @@ import { MessengerContext } from "../../../context/Provider";
 import PresenceDot from "./PresenceDot";
 
 import "./UserList.scss";
+import useWindowDimensions from "../../../hooks/useWindowDimensions";
+import useClickOutside from "../../../hooks/useClickOutside";
 
 interface Props {
   setUser: React.Dispatch<SetStateAction<User | null>>;
@@ -14,9 +22,14 @@ interface Props {
 
 const UserList = ({ setUser }: Props) => {
   const [users, setUsers] = useState<User[]>();
+  const [mobileShow, setMobileShow] = useState(false);
+
+  const buttonRef = useRef<HTMLButtonElement>(null);
 
   const { currentUser, setCurrentChat, currentChat } =
     useContext<any>(MessengerContext);
+
+  const { width } = useWindowDimensions();
 
   useEffect(() => {
     const users = db
@@ -49,24 +62,42 @@ const UserList = ({ setUser }: Props) => {
     return id;
   };
 
+  useClickOutside(buttonRef, () => setMobileShow(false));
+
   return (
-    <div className="users-list">
-      {users &&
-        users.map((user) => (
-          <div
-            className={`user-wrap ${
-              getChatId(user.uid) === currentChat ? "user-wrap_active" : ""
-            } `}
-            key={user.uid}
-            onClick={() => currentChatHandler(user)}
-          >
-            <h2>
-              {user.name}
-              <PresenceDot uid={user.uid} />
-            </h2>
-          </div>
-        ))}
-    </div>
+    <>
+      {width < 768 && (
+        <button
+          className="users-list__open-btn"
+          onClick={() => setMobileShow(!mobileShow)}
+          ref={buttonRef}
+        >
+          Users
+        </button>
+      )}
+      <div
+        className={`users-list ${width < 768 ? "users-list__mobile" : ""} ${
+          mobileShow ? "users-list__mobile_active" : ""
+        }`}
+        onClick={(e: React.MouseEvent<HTMLDivElement>) => e.stopPropagation()}
+      >
+        {users &&
+          users.map((user) => (
+            <div
+              className={`user-wrap ${
+                getChatId(user.uid) === currentChat ? "user-wrap_active" : ""
+              } `}
+              key={user.uid}
+              onClick={() => currentChatHandler(user)}
+            >
+              <h2>
+                {user.name}
+                <PresenceDot uid={user.uid} />
+              </h2>
+            </div>
+          ))}
+      </div>
+    </>
   );
 };
 
