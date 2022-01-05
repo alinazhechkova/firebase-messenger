362b965a644bba42941a8abd54adbb0d90b97d47
Alina Zhechkova <alinazhech@gmail.com>
2022-01-05 09:41:07 +0200

    Refactored rules

--- src/context/Provider.tsx
+++ src/context/Provider.tsx
@@ -1,6 +1,6 @@
-import firebase from "firebase/compat";
 import React, { createContext, useEffect, useState } from "react";
 
+import firebase from "firebase/compat";
 import { auth, db, realtime } from "../firebase";
 
 export const MessengerContext = createContext<any>(null);
